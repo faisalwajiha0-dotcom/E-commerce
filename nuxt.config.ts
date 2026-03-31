596bdfa2073ae35a962d39201573ab2d0771a3d5
faisalwajiha0-dotcom <faisalwajiha0@gmail.com>
2026-03-31 18:37:09 +0500

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  eslint: { config: { stylistic: true } },
 })
