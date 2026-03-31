4ff5ae8c955c198ec21eefc12c8b9bfbec6325d8
faisalwajiha0-dotcom <faisalwajiha0@gmail.com>
2026-03-31 23:34:04 +0500

    up

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const links = [
+  { label: 'Home', to: '/' },
+  { label: 'About ', to: '/about' },
+
+]
+</script>
+
+<template>
+  <UMain>
+    <UHeader>
+      <!-- logo -->
+      <template #title>
+        <Logo class="font-bold text-3xl">
+          E-commerce
+        </Logo>
+      </template>
+
+      <!-- Desktop Navigation -->
+      <nav class="hidden md:flex items-center gap-6">
+        <NuxtLink
+          v-for="link in links"
+          :key="link.to"
+          :to="link.to"
+          class="text-sm font-medium hover:text-primary transition-colors"
+          active-class="text-primary">
+          {{ link.label }}
+        </NuxtLink>
+      </nav>
+
+      <template #right>
+        <UColorModeButton />
+      </template>
+    </UHeader>
+  </UMain>
+</template>
