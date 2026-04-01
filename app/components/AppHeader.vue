a1f2d8e1084e5da23f6db33924a400767b64d09c
faisalwajiha0-dotcom <faisalwajiha0@gmail.com>
2026-04-01 15:37:03 +0500

    up

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 const links = [
   { label: 'Home', to: '/' },
-  { label: 'About ', to: '/about' },
-
+  { label: 'Product ', to: '/product' },
+  { label: 'ProductCard ', to: '/productcard' },
 ]
 </script>
 
@@ -22,7 +22,7 @@ const links = [
           v-for="link in links"
           :key="link.to"
           :to="link.to"
-          class="text-sm font-medium "
+          class="text-sm font-medium hover:text-primary transition-colors"
           active-class="text-primary">
           {{ link.label }}
         </NuxtLink>
