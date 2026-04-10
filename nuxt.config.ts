6ee3d41d1a0c15340af29f292b76c4ec17d3b274
faisalwajiha0-dotcom <faisalwajiha0@gmail.com>
2026-04-10 12:17:17 +0500

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxthub/core'
+  ],
   devtools: { enabled: true },
-  app: {
-    head: {
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
-        }
-      ]
-    }
-  },
   css: ['~/assets/css/main.css'],
+  ui: {
+    content: true,
+    experimental: { componentDetection: true }
+  },
   compatibilityDate: '2025-07-15',
+
+  nitro: {
+    preset: 'cloudflare_module',
+    cloudflare: { deployConfig: true, nodeCompat: true }
+  },
+  hub: { db: 'sqlite' },
   eslint: { config: { stylistic: true } },
+
 })
