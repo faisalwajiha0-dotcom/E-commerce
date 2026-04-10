a291246ce12fc9544512cb6b01ab0666105e2367
faisalwajiha0-dotcom <faisalwajiha0@gmail.com>
2026-04-10 13:18:58 +0500

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,16 @@ export default defineNuxtConfig({
     '@nuxthub/core'
   ],
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+        }
+      ]
+    }
+  },
   css: ['~/assets/css/main.css'],
   ui: {
     content: true,
